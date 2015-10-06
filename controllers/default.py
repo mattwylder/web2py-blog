d039b7095e0b8afa2d9cd723b31da9fec252883b
Matthew Wylder <matt@wylder.us>
2015-10-06 01:05:04 -0500

    Initial commit

---
+++ controllers/default.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    posts = db().select(db.post.ALL);
+    return dict(posts=posts)
+
+@auth.requires_login()
+def create():
+    form = SQLFORM(db.post)
+    if form.process().accepted:
+        response.flash = 'Your blog was posted'
+    return dict(form=form)
+
+#@auth.requires_login()
+def show():
+    #TODO: Allow editing/ deletion if you're the user who created this
+    post = db.post(request.args(0,cast=int)) or redirect(URL('index'))
+    title=post.title
+    user_id=post.user_id
+    body = post.body
+    comments = db(db.comment.post_id==post.id).select()
+    form = SQLFORM(db.comment)
+    if form.process().accepted:
+        response.flash = 'Comment posted'
+    return dict(title=title, username=user_id, user_id=user_id, body=body, comments=comments, form=form)
+
+def profile():
+    user_id = request.args(0, cast=int)
+    user = db(db.auth_user.id == user_id).select();
+    posts = db(db.post.user_id == user_id).select();
+    return dict(user_id=user_id, posts=posts)
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
